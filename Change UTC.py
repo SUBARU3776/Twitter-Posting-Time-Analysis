6acda7be50bb57d990f08a6b9adaf1e9a23731f7
_SUBARU_3776_ <71259928+SUBARU3776@users.noreply.github.com>
2023-05-19 23:21:35 +0900

    Update Change UTC.py

--- Change UTC.py
+++ Change UTC.py
@@ -1,26 +1,26 @@
 import pandas as pd
 from datetime import timedelta
 
-# データを読み込む
+# Read data
 df = pd.read_csv(r'hoge:\hogehoge.csv')
 
 # 'created_at'列を日時型に変換する
 df['created_at'] = pd.to_datetime(df['created_at'])
 
 # 日本時間から現地時間に変換する
-df['created_at'] = df['created_at'] - timedelta(hours=**)  # 日本時間から時差分を引く
-df['created_at'] = df['created_at'].dt.tz_localize('Asia/Tokyo')  # 日本時間をタイムゾーンとして設定
-df['created_at'] = df['created_at'].dt.tz_convert('hogehoge/hoge')  # 任意の現地時間に変換（例：Europe/Berlin）
+df['created_at'] = df['created_at'] - timedelta(hours=**)  # Subtract time difference from Japan time
+df['created_at'] = df['created_at'].dt.tz_localize('Asia/Tokyo')  # Set Japan time as time zone
+df['created_at'] = df['created_at'].dt.tz_convert('hogehoge/hoge')  # Convert to any local time (e.g. Europe/Berlin)
 
-# 日付のフォーマットを変更する
+# Change the date format
 df['created_at'] = df['created_at'].dt.strftime("%Y/%m/%d %H:%M:%S")
 
-# 結果を表示する
+# Display results
 print(df['created_at'])
 
-# ファイル名を生成する
+# generate a file name
 file_name = pd.Timestamp.now().strftime("%Y%m%d%H%M%S") + "_converted.csv"
 
-# データをCSVファイルとして保存する
-df.to_csv(file_name, index=False) # ファイル名は適当に指定してください
-print("変換後のデータをCSVファイルとして保存しました。ファイル名: " + file_name) 
+# Save data as a CSV file
+df.to_csv(file_name, index=False) # Please specify the file name as appropriate.
+print("Saved converted data as CSV file. File name:" + file_name) 
